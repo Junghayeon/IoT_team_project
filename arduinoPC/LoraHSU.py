b454914d90061820919f515eef6d18d72bfcb4d0
Junghayeon <jennyjhy@naver.com>
2022-12-12 16:24:30 +0900

    modified PC arduino

--- arduinoPC/LoraHSU.py
+++ arduinoPC/LoraHSU.py
@@ -3,13 +3,12 @@ import serial
 import os
 
 os.system('"arduino-cli"')
+# 해당 경로명은 LoraHSU_PCarduino.ino 파일의 경로명으로 재설정
 os.system('"arduino-cli compile C:\\Users\\jessi\\git\\IoTTeamProject\\arduinoPC"')
 os.system('"arduino-cli upload C:\\Users\\jessi\\git\\IoTTeamProject\\arduinoPC"')
 
-# port : 앞에서 arduino ide에서 확인한 포트 이름을 입력합니다
-# baudrate : 아두이노 setup함수에서 serial.begin에 사용된 값을 사용합니다
-# 포트가 연결되면 보드가 껐다 켜집니다
-# 초기화가 완료될 시간을 약간 기다려줍니다
+# port : 로라가 보내준 데이터에서 포트번호 확인
+# baudrate : 아두이노 setup함수에서 serial.begin에 사용된 값을 사용
 arduino = serial.Serial(port = "COM12", baudrate = 9600)
 time.sleep(1)
 
@@ -24,9 +23,12 @@ arduino.close()
 data2 = str(data1)
 data3 = str(data2[0:500])
 
+# 읽어온 경로명 가공하기
 path = data3.split("\\r\\n")
-
-print(path[1])
+path1 = str(path).split("g")
 
 result = path[1].replace("\\\\", "\\")
-os.system(result)
+final_result = path1[1].replace("'","")
+
+# 해당 경로명에서 파일 열기
+os.system(final_result)
